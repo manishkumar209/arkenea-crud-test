9e51d9f23ca79dbd3d8e4485898c546f33cd9e6c
Manish Kumar <manishjaiinfoway@gmail.com>
2022-08-07 11:01:06 +0530

    UI, API also screenshot added

---
+++ angular-ui/src/app/app.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../_service/app.service';
+import { MatDialog } from "@angular/material/dialog";
+import { UserModalComponent } from './user-modal/user-modal.component';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  Users = [{
+    _id: '',
+    fname: '',
+    lname: '',
+    email: '',
+    phone_number: '',
+    profile_image: '',
+  }];
+
+  constructor(
+    private apiService: ApiService,
+    public dialog: MatDialog,
+    ){
+
+  }
+  ngOnInit(){
+    this.getUsers();
+  }
+  getUsers(){
+    this.apiService.getUsers()
+    .subscribe((result: any) => {
+      if(result.success){
+        this.Users = result.data
+      }
+    })
+  }
+  get(id: String){
+    this.dialog.open(UserModalComponent, {
+      width: '1000px',
+      data: {
+        text: 'View Details',
+        type: 'view',
+        id: id
+      }
+      }).afterClosed().subscribe(result => {
+    });
+  }
+  add(){
+    this.dialog.open(UserModalComponent, {
+      width: '1000px',
+      data: {
+        text: 'Add User',
+        type: 'add',
+      }
+      }).afterClosed().subscribe(result => {
+      this.getUsers();
+    });
+  }
+  update(id: any){
+    this.dialog.open(UserModalComponent, {
+      width: '1000px',
+      data: {
+        text: 'Update User',
+        type: 'update',
+        id: id
+      }
+      }).afterClosed().subscribe(result => {
+      this.getUsers();
+    });
+  }
+  detele(id: any){
+    this.apiService.deletUser(id)
+    .subscribe((result: any) => {
+      this.getUsers();
+    })
+  }
+}
